a7b8597aaf4f979e63425a0917f94861f6539374
Raquel Braga <87723504+Raquelsantos242@users.noreply.github.com>
2022-09-06 16:48:46 -0300

    Delete webscraping1.py

--- webscraping1.py
+++
@@ -1,9 +0,0 @@
-import urllib3
-
-url = 'https://raw.githubusercontent.com/edubd/bd/main/paises.csv'
-
-http = urllib3.PoolManager()
-response = http.request('GET', url)
-
-csv = response.data
-print(csv)
